20002a6033f2e0fed3795b71e719b87e59ebe67c
android_lcw <android_lcw@126.com>
2016-08-03 10:58:24 +0800

    finish

---
+++ eventbus/src/main/java/com/hfax/demo/eventbusdemo/EventMessage/MyEvent.java
@@ -0,0 +1,28 @@
+package com.hfax.demo.eventbusdemo.EventMessage;
+
+/**
+ * 作者： lcw on 2016/7/7.
+ * 博客： http://blog.csdn.net/lsyz0021/
+ */
+public class MyEvent {
+
+	public static class Message {
+
+		public String msg;
+
+		public Message(String msg) {
+			this.msg = msg;
+		}
+	}
+
+
+	public static class OtherMessage {
+		public String msg;
+
+		public OtherMessage(String msg) {
+			this.msg = msg;
+		}
+	}
+
+
+}
